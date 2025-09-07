f5c32989a957809195084ddb6e436a11b35f5d1c
LanceYang <1572534835@qq.com>
2025-09-07 17:25:19 +0800

    feat: 更新 mp-basic 模板

--- packages/create-onemp/templates/mp-basic/src/App.vue
+++ packages/create-onemp/templates/mp-basic/src/App.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { onHide, onLaunch, onShow } from '@dcloudio/uni-app';
+import { useAppStore } from '@/store';
+import { mpUpdate } from '@/utils/index';
+
+const appStore = useAppStore();
+
 onLaunch(() => {
-  console.log("App Launch");
+  console.log('App Launch');
+
+  // 初始化系统信息
+  appStore.initSystemInfo();
+
+  // #ifdef MP-WEIXIN
+  mpUpdate();
+  // #endif
 });
+
 onShow(() => {
-  console.log("App Show");
+  console.log('App Show');
 });
+
 onHide(() => {
-  console.log("App Hide");
+  console.log('App Hide');
 });
 </script>
-<style></style>
+
+<style lang="scss">
+/* 每个页面公共css */
+@import 'uview-plus/index.scss';
+@import '@/static/styles/common.scss';
+</style>
--- packages/create-onemp/templates/mp-basic/src/main.ts
+++ packages/create-onemp/templates/mp-basic/src/main.ts
@@ -1,7 +1,13 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import App from '@/App.vue';
+import setupPlugins from '@/plugins';
+// 引入UnoCSS
+import 'virtual:uno.css';
+
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(setupPlugins);
+
   return {
     app,
   };
